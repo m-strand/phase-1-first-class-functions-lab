f5f210574e43dce77bcfb5ba2402cbd9d48f061d
Brooke Strand <brooke.strand007@gmail.com>
2022-02-12 23:40:28 -0500

    commit tree 6666bc507fdef1b9640095d72ded087458c31f9a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driverArr) {
+    return driverArr[0,1];
+};
+
+const returnLastTwoDrivers = function (driverArr) {
+    return driverArr[driverArr.length-2, driverArr.length-1];
+};
+
+let selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+
+/*function createFareMultiplier () {
+
+}
+const fareDoubler = 
+
+const fareTripler = 
+
+function selectDifferentDrivers() { 
+
+}*/
